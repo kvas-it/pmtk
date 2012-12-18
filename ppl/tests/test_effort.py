f18054f71bc0cd9cd109146e987135d3cd8cfe50
Vasily Kuznetsov <kvas.it@gmail.com>
2012-12-18 15:01:07 +0100

    second effort estimation test

--- ppl/tests/test_effort.py
+++ ppl/tests/test_effort.py
@@ -12,12 +12,17 @@ from ppl.project import Project
 
 class EffortTest(unittest.TestCase):
 
-    def test_simple_tasks(self):
+    def test_simple_task(self):
         p = Project()
         p.addTask('a')
         p.addTask('b')
         p.addEstimate('a', 5)
         self.failUnlessEqual(p.getTotalEffort(), 5)
+        p.addTask('ba', parent='b')
+        p.addTask('bb', parent='b')
+        p.addEstimate('ba', 3)
+        p.addEstimate('bb', 2)
+        self.failUnlessEqual(p.getTotalEffort(), 10)
 
 
 if __name__ == '__main__':
